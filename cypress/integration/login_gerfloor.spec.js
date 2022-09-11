3c801400ff99c0b047514bac13f60ba011821f27
Charlles Macedo de Santana <charlles.santana@itriad.org.br>
2022-09-11 18:22:45 -0400

    commit 03

---
+++ cypress/integration/login_gerfloor.spec.js
@@ -0,0 +1,42 @@
+
+describe('Login gerfloor', function() {
+
+
+  it('apiLogin', function() {
+
+    cy.request('POST', 'https://sisw-dev.gertec.com.br:8080/api/Auth/login', {
+        login: Cypress.env('login'),
+        password: Cypress.env('senha'),
+        idCurrentBranch: '3',
+        }).then((response) => {
+          expect(response).property('status').to.equal(200)
+          cy.writeFile('cypress/fixtures/loginResponseBody.json', response.body)
+        })
+  })
+
+
+  it('getLogin', function() {
+
+    let szIdProfile
+    let szUsername
+    let szToken
+    let szIdCurrentBranch
+  
+    cy.readFile('cypress/fixtures/loginResponseBody.json').its('idProfile').then((idProfile) => { szIdProfile = idProfile }) 
+    cy.readFile('cypress/fixtures/loginResponseBody.json').its('login').then((userName) => { szUsername = userName}) 
+    cy.readFile('cypress/fixtures/loginResponseBody.json').its('token').then((token) => { szToken = token }) 
+    cy.readFile('cypress/fixtures/loginResponseBody.json').its('idCurrentBranch').then((idCurrentBranch) => { szIdCurrentBranch = idCurrentBranch}) 
+  
+    cy.visit('https://sisw-dev.gertec.com.br/login', {
+      onBeforeLoad:  (browser) => {
+        browser.localStorage.setItem('idProfile', szIdProfile);
+        browser.localStorage.setItem('username', szUsername);
+        browser.localStorage.setItem('token', szToken);
+        browser.localStorage.setItem('idCurrentBranch', szIdCurrentBranch);
+      }
+    });
+
+  })
+  
+
+})
